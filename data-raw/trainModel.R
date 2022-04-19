75d643d54e22d7b91ed194773efb0a3aa5f1c953
chen-6647 <chen.6647@osu.edu>
2022-04-19 14:32:12 -0500

    Update trainModel.R

--- data-raw/trainModel.R
+++ data-raw/trainModel.R
@@ -3,6 +3,29 @@
 # 1) Load internal data (only needed for last step)
 devtools::document()
 
+data <- readRDS('Z:/Current Studies/STAR Grant/Data/TrainingData/trainingData_2022-March-9.RDS')
+data <- data[,-1]
+
+# 2) Compute the 90% and 95% quantiles
+quantile90 <- apply(data,2,FUN=function(x) quantile(x,probs=0.9))
+quantile95 <- apply(data,2,FUN=function(x) quantile(x,probs=0.95))
+
+# 3) Compute the 1, 1.5, and 2 z-scores
+means <- apply(data,2,mean)
+sds <- apply(data,2,sd)
+
+compute_zscore <- function(data,cutpoint=1){
+  result <- rep(NA,dim(data)[2])
+  for(i in 1:dim(data)[2]){
+    result[i] <- cutpoint*sd(data[,i])+mean(data[,i])
+  }
+  result
+}
+
+zscore1 <- compute_zscore(data,1)
+zscore1.5 <- compute_zscore(data,1.5)
+zscore2 <- compute_zscore(data,2)
+
 
 # 2) Load training data
 data <- readRDS('Z:/Current Studies/STAR Grant/Data/TrainingData/trainingData_2022-March-9.RDS')
@@ -32,5 +55,6 @@ prc_cutoff <- cutoff(cut_values)
 
 
 # 7) Save results as internal data
-usethis::use_data(epsi_idas_questions, final_coefs, prc_cutoff,
+usethis::use_data(quantile90,quantile95,zscore1,zscore1.5,zscore2,
+                  epsi_idas_questions, final_coefs, prc_cutoff,
                   internal = TRUE, overwrite = TRUE)
