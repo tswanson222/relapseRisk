441020f23f428aa3936e4c513522e396b17d2edf
Yiyang Chen <chen.6647@osu.edu>
2023-01-09 15:54:02 -0600

    Update Assignment.R

--- R/Assignment.R
+++ R/Assignment.R
@@ -1,5 +1,14 @@
+# Read from STAR_therapist_startdate.csv file to get the therapist and startdate of each participant
+# Create separate files for each individual with the name: [therapist code]_[epoch code]_[participant id].csv
+# These csv files are then processed by functions in pilrdata.R to obtain ED factors and generate reports
+# Reports are generated under the name of [therapist code]_[epoch code]_[participant id].html
+# Convert all reports into the [therapist code]_[epoch code]_[participant id].pdf format to upload
 # Assigns new configurations for each participant
+# Create Bulk Config Upload file (.csv)
+# column headers: participant_id, epoch_code, config_code
 
 
-
+BESTU_assignment <- function(){
+  
+}
 
