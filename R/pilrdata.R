3e533b093d9a2d8dc88622db17ccc2e3f1e18fd1
Yiyang Chen <chen.6647@osu.edu>
2023-08-21 13:30:14 -0500

    Update pilrdata.R

--- R/pilrdata.R
+++ R/pilrdata.R
@@ -347,7 +347,7 @@ createReport <- function(participant, therapist=0, epoch=0, id=0, output = './',
 
 
 
-questionTable <- function(data, week, questions, participant_assignment){
+questionTable <- function(data, week, questions, participant_assignment,table_number){
   if(participant_assignment=="treat"){
     corres <- list()
   for(i in 1:3){
@@ -420,9 +420,7 @@ questionTable <- function(data, week, questions, participant_assignment){
   
   else {
       sub <- subset(data,substr(question_code,1,5)=="diary" & survey_code==paste0("diary_w",week))
-      if(dim(sub)[1]>12){
-        sub <- sub[1:12,]
-      }
+      sub <- sub[(8*table_numer-7):(8*table_number),]
       if(!is.null(sub) & dim(sub)[1]>0){
           tab <- data.frame(array(dim=c(100,1)))
           cols <- rep(NA,100)
